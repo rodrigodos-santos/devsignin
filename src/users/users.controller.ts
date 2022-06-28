862935a44d54b2f5e2707250bdab5962b6928725
Rodrigo dos Santos <rodrigo.contato@live.com>
2022-06-28 16:03:46 -0300

    feat: include methods to autenticate

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { SigninDto } from './dto/signin.dto';
+import { User } from './models/users.model';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Post('signup')
+  @HttpCode(HttpStatus.CREATED)
+  public async signup(@Body() signupDto: SigninDto): Promise<User> {
+    return this.userService.signup(signupDto);
+  }
+
+  @Post('signin')
+  @HttpCode(HttpStatus.OK)
+  public async signin(
+    @Body() signinDto: SigninDto,
+  ): Promise<{ name: string; jwtToken: string; email: string }> {
+    return this.userService.signin(signinDto);
+  }
+
+  @Get()
+  @UseGuards(AuthGuard('jwt'))
+  @HttpCode(HttpStatus.OK)
+  public async findAll(): Promise<User[]> {
+    return this.userService.findAll();
+  }
+}
